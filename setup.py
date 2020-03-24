c925966bf9868627973d8b0cb418fa128bb19c5b
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2020-03-24 11:18:07 -0400

    erm

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='JTools',  
      version='1.1',
-     scripts=['__init__.py','UsefullSnipits.py','TimeTools.py','MathTools.py','FileTools.py'] ,
+    
      author="Joshua Zack",
      author_email="jbot237@gmail.com",
-     description="Mysc Usfull tools",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/Quiltic/JTools",
-     packages=setuptools.find_packages(),
-     classifiers=[
+     
+    description="Mysc Usfull tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    url="https://github.com/Quiltic/JTools",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    
+    classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
