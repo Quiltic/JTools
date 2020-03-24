9c66196f8aea4d5fd28ed19269f5aaab8d635a34
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2020-03-24 11:23:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
 setuptools.setup(
      name='JTools',  
      version='1.1',
@@ -9,16 +8,15 @@ setuptools.setup(
      author_email="jbot237@gmail.com",
      
     description="Mysc Usfull tools",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    license='MIT',
     
     url="https://github.com/Quiltic/JTools",
     packages=setuptools.find_packages(),
     include_package_data = True,
     
     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
+         "Programming Language :: Python :: 3,
          "Operating System :: OS Independent",
      ],
  )
