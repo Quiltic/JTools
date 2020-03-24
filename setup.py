95052d9d0738bd209043845e3e875cab39885b8b
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2020-03-24 11:00:05 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='JTools',  
+     version='1.1',
+     scripts=['__init__','UsefullSnipits','TimeTools','MathTools','FileTools'] ,
+     author="Joshua Zack",
+     author_email="jbot237@gmail.com",
+     description="Mysc Usfull tools",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Quiltic/JTools",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
