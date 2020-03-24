7ed3e23b0738a8cd1885e755efd9e8525696c6dd
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2020-03-24 11:25:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     include_package_data = True,
     
     classifiers=[
-         "Programming Language :: Python :: 3,
+         "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
      ],
  )
